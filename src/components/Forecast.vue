c871381ff56571b9ef4ec28bff82cc2cb4cd549b
reiv003 <reiv003@student.kristiania.no>
2022-03-16 07:57:13 +0100

    Got data import working, though forecast is displayed individually with indices instead of a loop

---
+++ src/components/Forecast.vue
@@ -0,0 +1,77 @@
+<template>
+	<h1>Værmelding</h1>
+	<div> {{ city }} </div>
+	<br>
+	<div> {{ tempToday }} </div>
+	<div> {{ windToday }} </div>
+	<div> {{ descToday }} </div>
+	<br>
+	<div> {{ forecastTemp1 }} </div>
+	<div> {{ forecastWind1 }} </div>
+	<br>
+	<div> {{ forecastTemp2 }} </div>
+	<div> {{ forecastWind2 }} </div>
+	<br>
+	<div> {{ forecastTemp3 }} </div>
+	<div> {{ forecastWind3 }} </div>
+	<button @click="changeCity">Endre by </button>
+	
+</template>
+
+<script>
+	export default {
+		
+		data() {
+			return {
+				city: 'stavanger',
+				forecastTemp1: '',
+				forecastWind1: '',
+				forecastTemp2: '',
+				forecastWind2: '',
+				forecastTemp3: '',
+				forecastWind3: '',
+				forecast: [],
+				tempToday: '',
+				windToday: '',
+				descToday: ''
+
+			}
+		},
+
+		created() {
+			this.fetchNew();
+		},
+
+		methods: {
+			async fetchNew() {
+				let url = (`https://goweather.herokuapp.com/weather/${this.city}`);
+				const res = await fetch(url);
+				const { temperature, wind, description, forecast } = await res.json();
+				console.log(res);
+				console.log(temperature);
+				console.log(wind);
+				console.log(forecast);
+				console.log(description);
+				this.forecastTemp1 = forecast[0].temperature;
+				this.forecastWind1 = forecast[0].wind;
+				this.forecastTemp2 = forecast[1].temperature;
+				this.forecastWind2 = forecast[1].wind;
+				this.forecastTemp3 = forecast[2].temperature;
+				this.forecastWind3 = forecast[2].wind;
+				this.tempToday = temperature;
+				this.descToday = description;
+				this.windToday = wind;
+				
+		
+			},
+
+			changeCity() {
+				this.city = 'bergen';
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>	
